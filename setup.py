85cd8085d3b60f34b3c4182599dbce529579c158
shardul <shardulmehetar07@gmail.com>
2021-09-26 01:38:45 +0530

    setup completed

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME = "oneNeuron_pypi"
+USER_NAME = "shardul0701"
+
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.1",
+    author=USER_NAME,
+    author_email="shardulmehetar07@gmail.com",
+    description="it's a implementation of Perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy"
+        "tqdm"
+        "logging"
+        "pandas"
+        "joblib"
+        "matplotlib"
+    ]
+)
